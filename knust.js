44c70c34ec59a54e56aa2d984aeab746f601eeb3
kwakner <amenyofrancis27@gmail.com>
2023-09-17 21:33:16 +0000

    first commit

---
+++ knust.js
@@ -0,0 +1,32 @@
+// Active selection for HelpDesk
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myChoice");
+var deks = header.getElementsByClassName("desk-item");
+for (var i = 0; i < deks.length; i++) {
+  deks[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//  Toggle HelpDesk
+function toggleDesky(deskIndex) {
+  let desks = document.getElementsByClassName("serve");
+  for (let i = 0; i < desks.length; i++) {
+    desks[i].style.display = "none";
+  }
+  desks[deskIndex].style.display = "flex";
+
+  // REMOVE ACTIVE DESK-ITEM FROM ALL HELPDESK
+  let list = document.getElementsByClassName("sub");
+  for (let i = 0; i < list.length; i++) {
+    list[i].classList.remove("active");
+  }
+
+  // ADD THE ACTIVE CLASS TO THE CLICKED ITEM
+  list[deskIndex].classList.add("active");
+}
+
+toggleDesky(0);
